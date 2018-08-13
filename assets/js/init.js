6ea935f106f17e518a2cb06b16502559892dd06d
lucasfloriani <lucas.floriane@gmail.com>
2018-08-12 21:14:56 -0300

    Test frequent request to get info about granja

--- assets/js/init.js
+++ assets/js/init.js
@@ -21,17 +21,34 @@ const getById = id => document.getElementById(id);
           <div class="card-content white-text">
             <p class="card-title rem-3" id="js-card-main-title">${title}</p>
             <p class="card-title card-custom-title">
-              <i class="far fa-lightbulb yellow-text text-darken-1 rem-2"></i> Luminosidade: ${luminosidade}
+              <i class="far fa-lightbulb yellow-text text-darken-1 rem-2"></i> Luminosidade: ${luminosidade}%
             </p>
             <p class="card-title card-custom-title">
-              <i class="fas fa-thermometer-empty deep-orange-text text-darken-2 rem-2"></i> Temperatura: ${temperatura}
+              <i class="fas fa-thermometer-empty deep-orange-text text-darken-2 rem-2"></i> Temperatura: ${temperatura}C
             </p>
             <p class="card-title card-custom-title">
-              <i class="fas fa-tint light-blue-text text-darken-1 rem-2"></i> Umidade: ${umidade}
+              <i class="fas fa-tint light-blue-text text-darken-1 rem-2"></i> Umidade: ${umidade}%
             </p>
           </div>
         </div>
       </div>
     `;
+    granjas.innerHTML = template;
   }
+
+  setInterval(() => {
+      fetch("http://127.0.0.1/edsa-projeto-nodemcu?controller=granja&action=getGranjaLastInfo")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        addGranjaToPage(
+          granjas,
+          "Granja 1",
+          data.luminosidade.luminosidade,
+          data.temperatura.temperatura,
+          data.umidade.umidade
+        )
+      })
+      .catch(err => console.log(err))
+    }, 5000);
 })();
--- views/pages/home.php
+++ views/pages/home.php
@@ -1,24 +1,7 @@
 <?php require_once('views/partials/header.php') ?>
 <section>
-	<div class="container" id="js-granjas">
-		<div class="row">
-			<div class="col s12 m4">
-				<div class="card blue-grey darken-1">
-					<div class="card-content white-text">
-						<p class="card-title rem-3" id="js-card-main-title"></p>
-						<p class="card-title card-custom-title">
-							<i class="far fa-lightbulb yellow-text text-darken-1 rem-2"></i> Luminosidade: <span id="js-luminosidade"></span></span>
-						</p>
-						<p class="card-title card-custom-title">
-							<i class="fas fa-thermometer-empty deep-orange-text text-darken-2 rem-2"></i> Temperatura: <span id="js-temperatura"></span>
-						</p>
-						<p class="card-title card-custom-title">
-							<i class="fas fa-tint light-blue-text text-darken-1 rem-2"></i> Umidade: <span id="js-umidade"></span>
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
+	<div class="container">
+		<div class="row" id="js-granjas"></div>
 	</div>
 </section>
 
@@ -28,23 +11,33 @@
 			<div class="row">
 				<div class="col s12 m4">
 					<div class="icon-block">
-						<h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
-						<h5 class="center">Speeds up development</h5>
-						<p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+						<h2 class="center teal-text"><i class="fas fa-user-clock"></i></h2>
+						<h5 class="center">Análise em Tempo Real</h5>
+						<p class="light">
+							Ao unir a tecnologia junto com a produção avícula, podemos contar com uma análise
+							mais precisa da situação de nossas granjas, podendo produzir com mais tranquilidade
+							e eficiência.
+						</p>
 					</div>
 				</div>
 				<div class="col s12 m4">
 					<div class="icon-block">
-						<h2 class="center brown-text"><i class="material-icons">group</i></h2>
-						<h5 class="center">User Experience Focused</h5>
-						<p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+						<h2 class="center teal-text"><i class="fab fa-buromobelexperte"></i></h2>
+						<h5 class="center">Multiplos galpões</h5>
+						<p class="light">
+							Podemos adicionar vários galpões para serem analisados, aumentando assim a escalabilidade
+							do projeto, podendo tambem adicionar novas variaveis do ambiente para análise mais rapidamente.
+						</p>
 					</div>
 				</div>
 				<div class="col s12 m4">
 					<div class="icon-block">
-						<h2 class="center brown-text"><i class="material-icons">settings</i></h2>
-						<h5 class="center">Easy to work with</h5>
-						<p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+						<h2 class="center teal-text"><i class="fas fa-mobile-alt"></i></h2>
+						<h5 class="center">Multiplataforma</h5>
+						<p class="light">
+							Sistema desenvolvido para aceitar acesso dos demais tipos de despositivos,
+							como celulares, notebooks e computadores.
+						</p>
 					</div>
 				</div>
 			</div>
